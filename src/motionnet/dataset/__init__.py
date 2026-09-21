7b01a12925e510ce871871436aeec57530fb7abc
Santiago <santigmaya@gmail.com>
2026-09-20 22:14:25 -0500

    optimizing for lambda sweeps

--- src/motionnet/dataset/__init__.py
+++ src/motionnet/dataset/__init__.py
@@ -4,4 +4,5 @@ from .prefilter import (build_coeff_cache, PrefilterConfig,
 from .vanhateren_utils import split_images, load_iml
 from .plotting import show_hdr, animate_sample, plot_examples
 from .caching import DiskCachedDataset
+from .prepare import prepare_data
 from .resident_evil import materialize
